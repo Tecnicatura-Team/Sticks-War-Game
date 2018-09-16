960e241c6e37b6d1e69f38a6b473f52002382ae4
The-Barba <cristianfmazziotti@gmail.com>
2018-09-16 20:33:47 -0300

    Inicio del sitio

---
+++ ValidarConexion.php
@@ -0,0 +1,22 @@
+<?php
+//conectar con base de datos
+require("ConectarDB.php");
+
+//consulta
+$sql="select * from usuario where nombre='".$_REQUEST['Nombre']."'
+and contrasena='".$_REQUEST['Contraseña']."';";
+
+/*ejecuta la consulta*/
+$registros=mysqli_query($conexion, $sql)or die("Problemas en
+la selección de la base de datos".mysqli_error($conexion));
+
+if(mysqli_fetch_row($registros)!=0){
+  echo "<script>alert('Login satisfactorio');</script>";
+  //redirige a la pantalla principal del juego
+  echo "<script>location.href='juego.php';</script>";
+}else{
+  echo "<script>alert('No se pudo loguear');</script>";
+   //redirige a la pagina principal
+  echo "<script>location.href='SticksWar.php';</script>";
+}
+?>
---
+++ juego.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Sticks War Game</title>
+  </head>
+  <body>
+    <h1>Sticks War Game</h1>
+    <article >
+      <p>Juego</p>
+    </article>
+  </body>
+</html>
