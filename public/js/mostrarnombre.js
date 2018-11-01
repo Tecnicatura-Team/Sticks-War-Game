fd521fb0b385e37afd9d16903702ae567b405101
The-Barba <cristianfmazziotti@gmail.com>
2018-11-01 20:45:58 -0300

    menu arreglado

--- public/js/mostrarnombre.js
+++ public/js/mostrarnombre.js
@@ -4,36 +4,38 @@
 var socket = io.connect("http://127.0.0.1:3700")
 
 $(document).ready(
-    mostrarnombre()
-)
-// $("#pepe").click(alert("hola"))
+        mostrarnombre()
+    )
+    // $("#pepe").click(alert("hola"))
 
 function mostrarnombre() {
     ajax("./Controlador/MostrarNombre.php", true, "mostrarnombre")
-    // $("#closeB").hide()
+        // $("#closeB").hide()
     socket.on("mostrarnu", function(data) {
         // console.log(data)
         // console.log("'"+data.trim()+"'")
-        
+
         // console.log(data.trim().length)
-        if(data.trim()){
-            
-            $("#closeB").show()
+        if (data.trim()) {
+            $(".opPj").css("display", "block")
+            $("#closeB").css("display", "block")
             $(".close").html(data.replace(/"/g, ''))
-            // alert("Lleno")
-        }// }else{
-        //     alert("vacio")
-        // }
-        
+                // alert("Lleno")
+        } else {
+            $(".opPj").css("display", "none")
+                // console.log("vacio")
+        }
+
     })
 }
-function cerrarsesion(){
-   
+
+function cerrarsesion() {
+
     ajax("./Controlador/CerrarSesion.php", true, "cerrarsesion")
-    location.href="index.html"
-    // var nom =  $(".close").html()
-    // socket.on("cerrarsesion"+nom, function(data) {
-       
+    location.href = "index.html"
+        // var nom =  $(".close").html()
+        // socket.on("cerrarsesion"+nom, function(data) {
+
     // })
 
 }
